7d54b33a5a3f2ee5d0d900eb17d287d60c2e6eb5
brianbreitsch <brianbreitsch@gmail.com>
2015-03-04 17:47:00 -0700

    modified link script

--- link.sh
+++ link.sh
@@ -1,3 +1,9 @@
 # links to home directory
-ln -s vim ~/.vim
-ln -s vimrc ~/.vimrc
+if [ -e ~/.vimrc ]; then
+    rm ~/.vimrc;
+fi;
+if [ -e ~/.vim ]; then
+    rm -fr ~/.vim;
+fi;
+ln -s `pwd`/vim ~/.vim
+ln -s `pwd`/vimrc ~/.vimrc
